b1766d842cd663dbbdb182b2766de2c141e9f836
Terry S <terrysdev83779@gmail.com>
2022-01-19 13:32:30 +1100

    fixed data

--- R/rand_pokemon.R
+++ R/rand_pokemon.R
@@ -3,7 +3,7 @@
 
 #' @export
 rand_pokemon <- function(n = 1) {
-    pokemon_names <- readRDS(here::here("Data", "pokemon_en.rds"))
+    pokemon_names <- readRDS(system.file("extdata", "pokemon_en.rds", package = "randPokemon", mustWork = TRUE))
     stopifnot("n must be positive number" = n >= 1)
     stopifnot("n musn't exceed number of total pokemon" = n <= length(pokemon_names))
     return(sample(pokemon_names, n, replace = FALSE))
