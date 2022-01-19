219a6d0e25c2b581a9828e3ada7078cd68818bb2
Terry S <terrysdev83779@gmail.com>
2022-01-19 13:23:01 +1100

    export with doxygen

--- R/rand_pokemon.R
+++ R/rand_pokemon.R
@@ -2,7 +2,7 @@
 
 
 #' @export
-rand_pokemon <- function(n) {
+rand_pokemon <- function(n = 1) {
     pokemon_names <- readRDS(here::here("Data", "pokemon_en.rds"))
     stopifnot("n must be positive number" = n >= 1)
     stopifnot("n musn't exceed number of total pokemon" = n <= length(pokemon_names))
